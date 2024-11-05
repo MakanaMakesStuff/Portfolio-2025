1e836844ff070c3462afd6cf5c628bc816eb1e13
Makana Edwards <makrigamedeveloping@gmail.com>
2024-11-05 14:06:10 +0000

    backing up

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,10 @@
+const { cwd } = require("process")
+
 // ecosystem.config.js
 module.exports = {
 	apps: [
 		{
+			cwd: ".",
 			name: "frontend",
 			script: "npm run start",
 			args: "start",
@@ -18,7 +21,8 @@ module.exports = {
 			ref: "origin/staging",
 			repo: "git@github.com:MakanaMakesStuff/Portfolio-2025.git",
 			path: "/home/portfolio/current",
-			"post-deploy": "sh scripts/deploy.sh staging",
+			"post-deploy":
+				"cd ~/home/portfolio/source && sh scripts/deploy.sh staging",
 			// "post-setup": "cd ./source && npm install || exit",
 			// "post-deploy": "cd ./source && sh scripts/deploy.sh staging",
 		},
